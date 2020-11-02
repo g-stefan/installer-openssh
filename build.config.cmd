993d8790bab6888e8c8c5341265d152c551044b5
Grigore Stefan <g_stefan@yahoo.com>
2020-11-02 02:32:19 +0200

    update

--- build.config.cmd
+++ build.config.cmd
@@ -3,4 +3,6 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
+set PRODUCT_NAME=installer-openssh
 set PRODUCT_VERSION=8.1.0.0
+set PRODUCT_BASE=openssh
--- build.installer.cmd
+++ build.installer.cmd
@@ -3,9 +3,9 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> installer openssh
-
 call build.config.cmd
+
+echo -^> installer %PRODUCT_NAME%
 
 if exist installer\ rmdir /Q /S installer
 mkdir installer
@@ -15,6 +15,6 @@ mkdir build
 
 makensis.exe /NOCD "util\openssh-installer.nsi"
 
-call grigore-stefan.sign "OpenSSH" "installer\openssh-%PRODUCT_VERSION%-installer.exe"
+call grigore-stefan.sign "OpenSSH" "installer\%PRODUCT_BASE%-%PRODUCT_VERSION%-installer.exe"
 
 if exist build\ rmdir /Q /S build
