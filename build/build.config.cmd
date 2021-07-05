72972f937ca2ec580815569129319b16893a2e59
Grigore Stefan <g_stefan@yahoo.com>
2021-07-05 17:23:09 +0300

    update

--- build/build.config.cmd
+++ build/build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-openssh
-set PRODUCT_VERSION=8.1.0.0
+set PRODUCT_VERSION=8.6.0.0
 set PRODUCT_BASE=openssh
