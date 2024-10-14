7a9b1646ba6ca13e56ee696e0b094d029df7289e
Grigore Stefan <g_stefan@yahoo.com>
2024-10-14 16:57:18 +0300

    Update

--- fabricare/vendor.js
+++ fabricare/vendor.js
@@ -7,10 +7,20 @@ messageAction("vendor");
 
 Shell.mkdirRecursivelyIfNotExists("vendor");
 
+var vendorSourceGit = "https://github.com/g-stefan";
+if (Shell.hasEnv("VENDOR_SOURCE_GIT")) {
+	vendorSourceGit = Shell.getenv("VENDOR_SOURCE_GIT");
+};
+
+var vendorSourceAuth = "";
+if (Shell.hasEnv("VENDOR_SOURCE_AUTH")) {
+	vendorSourceAuth = Shell.getenv("VENDOR_SOURCE_AUTH");
+};
+
 var vendor = "openssh-" + Project.version + "-win64-msvc-2022.7z";
 if (!Shell.fileExists("vendor/" + vendor)) {
-	var webLink = "https://github.com/g-stefan/vendor-openssh/releases/download/v" + Project.version + "/" + vendor;
-	var cmd = "curl --insecure --location " + webLink + " --output vendor/" + vendor;
+	var webLink = vendorSourceGit + "/vendor-openssh/releases/download/v" + Project.version + "/" + vendor;
+	var cmd = "curl --insecure --location " + webLink + " " + vendorSourceAuth + " --output vendor/" + vendor;
 	Console.writeLn(cmd);
 	exitIf(Shell.system(cmd));
 };
